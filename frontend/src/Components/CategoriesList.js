085bc16c14a41dce0f60f927653c22ef85e32e30
lilirdz <lilimrod@gmail.com>
2020-07-07 11:16:12 -0500

    working on categories

--- frontend/src/Components/CategoriesList.js
+++ frontend/src/Components/CategoriesList.js
@@ -1,7 +1,14 @@
 import React from "react";
+import CategoryCard from "../Components/CategoriesCard.js";
 
 export default class CategoryList extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {this.props.categories.map((category) => (
+          <CategoryCard category={category} />
+        ))}
+      </div>
+    );
   }
 }
--- frontend/src/Containers/CategoriesContainer.js
+++ frontend/src/Containers/CategoriesContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
-import SearchBar from "../Components/SearchBar.js";
+import CategoriesList from "../Components/CategoriesList.js";
 
 export default class CategoriesContainer extends React.Component {
+  state = {
+    categories: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/api/v1/categories")
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          categories: data,
+        })
+      );
+  }
   render() {
     return (
       <div>
-        <SearchBar />
+        <CategoriesList categories={this.state.categories} />
       </div>
     );
   }
