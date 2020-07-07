cd157c6a5b2a10ef0b0c539cd585dd2ade024b9b
github username <github email address>
2020-07-06 22:41:27 -0500

    update

--- frontend/src/components/LibraryPlaylistCard.js
+++ frontend/src/components/LibraryPlaylistCard.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 class LibraryPlaylistCard extends React.Component{
     render(){
+        
+        const embedUrl = `https://www.youtube.com/embed/${this.props.video.id.videoId}`;
        
         return(
             <div>  
              
                 {this.props.video.snippet.title}
-                 
+                <iframe src={embedUrl} />
         
             </div>
            
